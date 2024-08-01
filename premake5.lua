e07934f05c7cfb506f26a02e82c0599276769952
lknknm <108239558+lknknm@users.noreply.github.com>
2024-08-01 19:21:22 -0300

    first commit

---
+++ premake5.lua
@@ -0,0 +1,31 @@
+﻿-- premake5.lua
+----------------------------
+VULKAN_SDK = os.getenv("VULKAN_SDK")
+GLFW = os.getenv("GLFW")
+
+----------------------------
+workspace "vulkan-tutorial"
+   configurations { "Debug", "Release" }
+   architecture "x64"
+   
+----------------------------
+project "vulkan-tutorial"
+   kind "ConsoleApp"
+   language "C++"
+   targetdir "bin/%{cfg.buildcfg}"
+   cppdialect "c++17" -- apply to all toolsets and generators
+
+   files { "./src/*.h", "./src/*.cpp", "./src/*.hpp", "./src/*.cxx" }
+   
+   includedirs { "external/glfw-3.4.bin.WIN64/include", "%{VULKAN_SDK}/Include", "external/glm" }
+   libdirs { "external/glfw-3.4.bin.WIN64/lib-vc2022", "%{VULKAN_SDK}/Lib" }
+   links { "glfw3", "vulkan-1" }
+
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
+      symbols "Off"
