cbf6bd634536adb58c88504422c833e08d4928ba
Johannes Eckhoff <j.eckhoff@gmail.com>
2024-08-22 14:41:14 +0200

    Use namespace instead of class

--- src/input.hpp
+++ src/input.hpp
@@ -1,38 +1,35 @@
 #pragma once
 
 //----------------------------------------------------------------------------
-// Input handling implementation class. 
-class Input
+// Input handling implementation class.
+namespace Input
 {
-public:
-    
     //----------------------------------------------------------------------------
-    static bool isKeyDown(GLFWwindow* windowHandle, int keyCode)
+    bool isKeyDown(GLFWwindow* windowHandle, int keyCode)
     {
         int state = glfwGetKey(windowHandle, keyCode);
         return state == GLFW_PRESS || state == GLFW_REPEAT;
     }
 
     //----------------------------------------------------------------------------
-    static bool isKeyReleased(GLFWwindow* windowHandle, int keyCode)
+    bool isKeyReleased(GLFWwindow* windowHandle, int keyCode)
     {
         int state = glfwGetKey(windowHandle, keyCode);
         return state == GLFW_RELEASE;
     }
 
     //----------------------------------------------------------------------------
-    static bool isMouseButtonDown(GLFWwindow* windowHandle, int mButtonCode)
+    bool isMouseButtonDown(GLFWwindow* windowHandle, int mButtonCode)
     {
         int state = glfwGetMouseButton(windowHandle, mButtonCode);
         return state == GLFW_PRESS;
     }
 
     //----------------------------------------------------------------------------
-    static glm::vec2 getMousePosition(GLFWwindow* windowHandle)
+    glm::vec2 getMousePosition(GLFWwindow* windowHandle)
     {
         double x, y;
         glfwGetCursorPos(windowHandle, &x, &y);
-        return { (float)x, (float)y };
+        return {(float)x, (float)y};
     }
-};
-
+}; // namespace Input
