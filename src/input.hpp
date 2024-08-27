c2297b7e498caf9cc641a752c6e352d1c9eb66aa
Johannes Eckhoff <j.eckhoff@gmail.com>
2024-08-27 10:16:41 +0200

    We have to add inline as this is no longer a class

--- src/input.hpp
+++ src/input.hpp
@@ -1,32 +1,34 @@
 #pragma once
 
 //----------------------------------------------------------------------------
-// Input handling implementation class.
+// Input handling implementation.
+#include <GLFW/glfw3.h>
+#include <glm.hpp>
 namespace Input
 {
     //----------------------------------------------------------------------------
-    bool isKeyDown(GLFWwindow* windowHandle, int keyCode)
+    inline bool isKeyDown(GLFWwindow* windowHandle, int keyCode)
     {
         int state = glfwGetKey(windowHandle, keyCode);
         return state == GLFW_PRESS || state == GLFW_REPEAT;
     }
 
     //----------------------------------------------------------------------------
-    bool isKeyReleased(GLFWwindow* windowHandle, int keyCode)
+    inline bool isKeyReleased(GLFWwindow* windowHandle, int keyCode)
     {
         int state = glfwGetKey(windowHandle, keyCode);
         return state == GLFW_RELEASE;
     }
 
     //----------------------------------------------------------------------------
-    bool isMouseButtonDown(GLFWwindow* windowHandle, int mButtonCode)
+    inline bool isMouseButtonDown(GLFWwindow* windowHandle, int mButtonCode)
     {
         int state = glfwGetMouseButton(windowHandle, mButtonCode);
         return state == GLFW_PRESS;
     }
 
     //----------------------------------------------------------------------------
-    glm::vec2 getMousePosition(GLFWwindow* windowHandle)
+    inline glm::vec2 getMousePosition(GLFWwindow* windowHandle)
     {
         double x, y;
         glfwGetCursorPos(windowHandle, &x, &y);
