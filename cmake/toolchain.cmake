5e930a2261a4943b5896d21639efac1e0fb7d4f6
Johannes Eckhoff <j.eckhoff@gmail.com>
2024-09-27 12:00:04 +0200

    Chainload toolchain file

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -1,6 +1,5 @@
-include(${CMAKE_CURRENT_LIST_DIR}/CompilerWarnings.cmake)
-set_project_warnings()
+include(${CMAKE_CURRENT_LIST_DIR}/Warnings.cmake)
+enable_warnings()
 
 include(${CMAKE_CURRENT_LIST_DIR}/Sanitizers.cmake)
-set(ENABLE_SANITIZER_ADDRESS YES)
 enable_sanitizers()
--- cmake/vcpkg.cmake
+++ cmake/vcpkg.cmake
@@ -8,4 +8,4 @@ FetchContent_Declare(
 FetchContent_MakeAvailable(vcpkg)
 
 set(CMAKE_TOOLCHAIN_FILE "${vcpkg_SOURCE_DIR}/scripts/buildsystems/vcpkg.cmake")
-# set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/toolchain.cmake")
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/toolchain.cmake")
