1bf45da1baaac7e8f467816d021cd12bdf2aef13
Johannes Eckhoff <j.eckhoff@gmail.com>
2024-09-27 19:04:53 +0200

    Update vcpkg git_tag to latest release
    Remove chainload file path

--- cmake/vcpkg.cmake
+++ cmake/vcpkg.cmake
@@ -2,10 +2,9 @@ include(FetchContent)
 FetchContent_Declare(
    vcpkg
    GIT_REPOSITORY https://github.com/microsoft/vcpkg
-   GIT_TAG 0789630513565c6ea73caad01f98313cdec1b073 # 2032.08.15
+   GIT_TAG 3508985146f1b1d248c67ead13f8f54be5b4f5da
 )
 
 FetchContent_MakeAvailable(vcpkg)
 
 set(CMAKE_TOOLCHAIN_FILE "${vcpkg_SOURCE_DIR}/scripts/buildsystems/vcpkg.cmake")
-set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/toolchain.cmake")
