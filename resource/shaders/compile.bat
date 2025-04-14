3b8cc46b799382ce8630e5e78d5ac0ce69975512
lknknm <github.snugness349@passinbox.com>
2025-04-14 22:07:57 +0200

    Fix: Cmake fix to build shaders

--- resource/shaders/compile.bat
+++
@@ -1,9 +0,0 @@
-C:\VulkanSDK\1.3.283.0\Bin\glslc.exe object.vert -o object_vertex.spv
-C:\VulkanSDK\1.3.283.0\Bin\glslc.exe texture.frag -o texture_fragment.spv
-C:\VulkanSDK\1.3.283.0\Bin\glslc.exe texture.frag -o texture_fragment.spv
-C:\VulkanSDK\1.3.283.0\Bin\glslc.exe solid_shading.frag -o solid_shading_fragment.spv
-C:\VulkanSDK\1.3.283.0\Bin\glslc.exe wireframe.frag -o wireframe_fragment.spv
-
-C:\VulkanSDK\1.3.283.0\Bin\glslc.exe grid.vert -o grid_vertex.spv
-C:\VulkanSDK\1.3.283.0\Bin\glslc.exe grid.frag -o grid_fragment.spv
-pause
