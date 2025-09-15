0f9a0495d21bdefa30bc5077caea390f931ce2b3
Johannes Eckhoff <j.eckhoff@gmail.com>
2025-09-15 18:41:00 +0200

    Update dependencies

--- cmake/vcpkg.cmake
+++ cmake/vcpkg.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 FetchContent_Declare(
    vcpkg
    GIT_REPOSITORY https://github.com/microsoft/vcpkg
-   GIT_TAG 26abb5b33f976246a5f2cdc45cdd073d51caff06
+   GIT_TAG 62efe42f53b1886a20cbeb22ee9a27736d20f149
 )
 
 FetchContent_MakeAvailable(vcpkg)
